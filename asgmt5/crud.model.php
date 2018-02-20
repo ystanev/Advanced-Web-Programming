7ed58b442414b51be35740c4eca102a82eb1924a
Yury <ystanev@yahoo.ca>
2018-02-20 18:44:29 -0500

    Add files via upload

--- asgmt5/crud.model.php
+++ asgmt5/crud.model.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('display_errors', 1); error_reporting(~0);
+//ini_set('display_errors', 1); error_reporting(~0);
 
 //Get all the data from the phonebook table, return as 2D array
 function getPhoneBook($conn){
@@ -21,4 +21,63 @@ function getPhoneBook($conn){
     }
 }
 
+//Delete record from the database
+function deletePhone($conn, $phoneid){
+    try{
+        //deleting a record from database
+        $stmt = $conn->prepare("DELETE FROM phonebook WHERE id=?");
+        $stmt->execute([$phoneid]);
+        var_dump($stmt);
+    }
+    //If detelePhone() fails, exit with failure status
+    catch(PDOException $e)
+    {
+        echo "deletePhone failed: " . $e->getMessage();
+        exit();
+    }
+}
+
+//Insert record into the database.
+function insertPhone($conn, $fname, $lname, $phone, $email,
+                     $location, $mc, $pos, $dept){
+    try{
+        $stmt = $conn->prepare("INSERT INTO phonebook" .
+                                "(id, fname, lname, phone, email, location, mc, pos, dept) VALUES" .
+                                "(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+        $stmt -> execute(["null", $fname, $lname, $phone, $email, $location, $mc, $pos, $dept]);
+    }
+    //If detelePhone() fails, exit with failure status
+    catch(PDOException $e)
+    {
+        echo "insertPhone failed: " . $e->getMessage();
+        exit();
+    }
+}
+
+//Editing Database Record
+function editPhone($conn, $phoneid,$fname, $lname, $phone, $email,
+                   $location, $mc, $pos, $dept){
+    try{
+        $stmt = $conn->prepare("UPDATE 
+                                    phonebook" .
+                               "SET 
+                                    fname=?, 
+                                    lname=?,
+                                    phone=?,
+                                    email=?,
+                                    location=?,
+                                    mc=?,
+                                    pos=?,
+                                    dept=?" .
+                                "WHERE 
+                                    id=?");
+        $stmt -> execute([$fname, $lname, $phone, $email, $location, $mc, $pos, $dept, $phoneid]);
+    }
+    catch(PDOException $e)
+    {
+        echo "editPhone failed: " . $e->getMessage();
+        exit();
+    }
+}
 ?>
--- asgmt5/crud.view.php
+++ asgmt5/crud.view.php
@@ -35,14 +35,28 @@
                 <td><?= $phonedata["mc"] ?></td>
                 <td><?= $phonedata["pos"] ?></td>
                 <td><?= $phonedata["dept"] ?></td>
+                <td><a href="crud.crtl.php?act=delete$id=<?= $phonedata["id"] ?>">DELETE</a></td>
             </tr>
             <?
         }
         ?>
     </table>
 
+    <h3>Add Phone Record</h3>
+    <form action="crud.crtl.php?act=insert" method="post">
+        <input type="text" name="f_name" placeholder="First Name"><br>
+        <input type="text" name="l_name" placeholder="Last Name"><br>
+        <input type="text" name="phone_num" placeholder="Phone #"><br>
+        <input type="text" name="e-mail" placeholder="E-Mail"><br>
+        <input type="text" name="location1" placeholder="Location"><br>
+        <input type="text" name="m_c" placeholder="MC"><br>
+        <input type="text" name="position" placeholder="Position"><br>
+        <input type="text" name="department" placeholder="Department"><br>
+        <input type="submit">
+    </form>
+
     <br><br>
     <h3>TPL Array Dump</h3>
-    <pre><? var_dump($TPL) ?></pre>
+    <pre><? //var_dump($TPL) ?></pre>
 </body>
 </html>
